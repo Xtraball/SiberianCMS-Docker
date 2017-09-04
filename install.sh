02edc041a61b5a733a4e5d80ee1f1a7117ee55a7
Alex-Yves CREPIN <a.crepin@xtraball.com>
2017-09-04 10:40:16 +0200

    feat: tiger docker

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-yum install -y git docker curl wget
-useradd siberian
+# Generates a temporary SSL certificate
+openssl genrsa -des3 -passout pass:x -out ./etc/ssl/default.pass.key 2048
+openssl rsa -passin pass:x -in ./etc/ssl/default.pass.key -out ./etc/ssl/default.key
+rm ./etc/ssl/default.pass.key
+openssl req -new -key ./etc/ssl/default.key -out ./etc/ssl/default.csr \
+  -subj "/C=FR/ST=Haute-Garonne/L=Toulouse/O=Xtraball/OU=SysAdmin/CN=mysiberian.example.com"
+openssl x509 -req -days 365 -in ./etc/ssl/default.csr -signkey ./etc/ssl/default.key -out ./etc/ssl/default.crt
 
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-mkdir -p /home/siberian
-cd /home/siberian
-git clone https://github.com/Xtraball/SiberianCMS-Docker.git ./docker
+# Generates a Random MySQL Password
+password='date +%s | sha256sum | base64 | head -c 16 ; echo'
+sed -i'' 's/MYSQL_ROOT_PASSWORD=changeme/'$password'/g' ./docker-compose.yml
