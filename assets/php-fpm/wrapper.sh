9d45d381e3c81d624c3bfa2608efcd7059c1a1b6
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-09-07 15:23:14 +0200

    feat(community): docker configuration for community

--- assets/php-fpm/wrapper.sh
+++ assets/php-fpm/wrapper.sh
@@ -3,24 +3,12 @@
 # Start php-fpm
 service php7.0-fpm start
 
-# Start cron
-service cron start
-
-# Start postfix
-service postfix start
-
 # Check if they are both running
 while /bin/true; do
   ps aux |grep php-fpm |grep -q -v grep
   PROCESS_1_STATUS=$?
 
-  ps aux |grep cron |grep -q -v grep
-  PROCESS_2_STATUS=$?
-
-  ps aux |grep postfix |grep -q -v grep
-  PROCESS_3_STATUS=$?
-
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0 ]; then
+  if [ $PROCESS_1_STATUS -ne 0 ]; then
     echo "One of the processes has already exited."
     exit -1
   fi
